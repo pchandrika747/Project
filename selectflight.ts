61af82139aff434012f0df83b3c3ccad21b4d090
vittanala poorna chandrika <71425699+pchandrika747@users.noreply.github.com>
2022-10-03 20:23:02 +0530

    Create selectflight.ts

---
+++ selectflight.ts
@@ -0,0 +1,10 @@
+export interface selectflight
+{
+    id:number
+    flightname:string
+    timefrom:number
+    timeto:number
+    duration:number
+    economyclassprice:number
+    businessclassprice:number
+}
