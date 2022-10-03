5a520aacf637554395a9ea85a02873882d51db2f
vittanala poorna chandrika <71425699+pchandrika747@users.noreply.github.com>
2022-10-03 20:21:37 +0530

    Create models>registration.ts

---
+++ models>registration.ts
@@ -0,0 +1,12 @@
+export interface Registration
+{
+    id:number
+    title:string
+    firstname:string
+    lastname:string
+    email:string
+    password:string
+    confirmpassword : string 
+    dateofbirth:Date
+    phonenumber:number
+}
